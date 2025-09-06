8397f0f55e2411ad30296cc49fbb49e6e3afe131
AsunaML <AsunaML@outlook.com>
2025-09-06 22:01:56 +0800

    save

--- tavern-script/handlers/ServerMessageHandler.ts
+++ tavern-script/handlers/ServerMessageHandler.ts
@@ -1,5 +1,5 @@
 import { MessageHandler } from "../../communication";
-import { ServerContext } from '../types';
+import { ServerContext } from '../ServerContext';
 
 // 新继承一个抽象类，确保服务端 Handler 都能使用 ServerContext 进行静态类型检查
 export abstract class ServerMessageHandler extends MessageHandler<ServerContext> {
